c1a321852f2c037230355823758ab5650d85ae5c
Gabriel Vieira <gabkv99@hotmail.com>
2024-02-07 11:09:10 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const novaTarefa = $('#tarefa').val();
+        const novoItem = $(`<li>${novaTarefa}</li>`);
+
+        $(novaTarefa).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).hide();
+        $(novoItem).fadeIn(1000);
+        $('#tarefa').val('');
+    })
+
+    $('ul').on('click', 'li', function(){
+        $(this).addClass('.risca-tarefa');
+    })
+
+    $('#botao-limpar').click(function(){
+        $('li').hide();
+    })
+})
